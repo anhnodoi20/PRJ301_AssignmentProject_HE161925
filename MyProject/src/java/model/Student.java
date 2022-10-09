df879f82cb07875bfdadc885a03e96b47f26f108
Luctdhe161925 <114924883+anhnodoi20@users.noreply.github.com>
2022-10-10 01:38:11 +0700

    ADD libs (10/10)
    add 2: jakarta.servlet.jsp.jstl and  jakarta.servlet.jsp.jstl-api

--- MyProject/src/java/model/Student.java
+++ MyProject/src/java/model/Student.java
@@ -4,6 +4,8 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author win
@@ -11,4 +13,60 @@ package model;
 public class Student {
     private String id;
     private String name;
+    private String gender;
+    private String email;
+    private Date dob;
+
+    public Student() {
+    }
+
+    public Student(String id, String name, String gender, String email, Date dob) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+    
+    
 }
